e55b33117f6758b11058eaddebd10f2b8d1594b0
Valentinee1 <66319230+Valentinee1@users.noreply.github.com>
2020-07-26 00:33:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+
+  // Видео
+  var video = document.getElementById ("video");
+
+  // кнопки
+  var playButton = document.getElementById ("play-pause");
+  var muteButton = document.getElementById ("mute");
+  var fullScreenButton = document.getElementById ("полный экран");
+
+  // Слайдеры
+  var seekBar = document.getElementById ("seek-bar");
+  var volumeBar = document.getElementById ("volume-bar");
+
+}
